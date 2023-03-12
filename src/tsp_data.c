c4a351685bf6de99839dff4ba3f47a076d198736
Udinanon <m.trapanotto@tutamail.com>
2023-03-12 11:07:34 +0100

    Correction on pointer checks

--- src/tsp_data.c
+++ src/tsp_data.c
@@ -2,7 +2,7 @@
 
 TSP_data* init_tsp_data(){
     TSP_data* data = (TSP_data* )malloc(sizeof(TSP_data));
-    if (data = NULL){
+    if(data == NULL){
         logger(FATAL, "TSP_data pointer allocation failed!");
         perror("Error printed by perror");
         exit(EXIT_FAILURE);
@@ -23,7 +23,7 @@ void allocate_points(TSP_data* data){
         exit(EXIT_FAILURE);
     }
     data->points = (Point*)malloc(sizeof(Point) * data->n_dimensions);
-    if (data->points = NULL) {
+    if (data->points == NULL) {
         logger(FATAL, "TSP_data points allocation failed!");
         perror("Error printed by perror");
         exit(EXIT_FAILURE);
