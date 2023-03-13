304cb86412a6caf45747722190b982d180174b4b
99lorenzospina <lorenzo.spina@studenti.unipd.it>
2023-03-13 17:56:10 +0100

    implementing random

--- src/tsp_data.c
+++ src/tsp_data.c
@@ -73,7 +73,7 @@ TSP_solution* NN(TSP_data* data){
         logger(FATAL, "TSP_solution cycle allocation failed!");
         perror("Error printed by perror");
         exit(EXIT_FAILURE);
-
+    }
     Point start = data->points[0];
     sol->cycle[0] = start;
     int cost = 0;
@@ -115,7 +115,8 @@ TSP_solution* NN(TSP_data* data){
     return sol;
 }
 
-TSP_solution* random_NN(TSP_data* data, double prob){
+TSP_solution* random_NN(TSP_data* data, double prob)
+{
     TSP_solution* sol = (TSP_solution *)malloc(sizeof(TSP_solution));   //allocate the solution
     if(sol == NULL){
         logger(FATAL, "TSP_solution pointer allocation failed!");
@@ -130,7 +131,7 @@ TSP_solution* random_NN(TSP_data* data, double prob){
         logger(FATAL, "TSP_solution cycle allocation failed!");
         perror("Error printed by perror");
         exit(EXIT_FAILURE);
-
+    }
     Point start = data->points[0];
     sol->cycle[0] = start;
     int cost = 0;
@@ -138,29 +139,38 @@ TSP_solution* random_NN(TSP_data* data, double prob){
     int* already_visited = (int*) calloc(data->n_dimensions, sizeof(int)); //all values to 0
 
     for (int j = 1; j < n; j++){
-        double row[n];
-        memcpy(row, &data->cost_matrix[index*n], n);
-        int present = 1;
-        int min_pos = 0;
-        while(present== 1){
-            for(int i = 1; i < n; i++){
-                if(row[i] < row[min_pos] && i!=index){
-                    min_pos = i;
+        int p = rand();
+        if(p>=prob){
+            double row[n];
+            memcpy(row, &data->cost_matrix[index*n], n);
+            int present = 1;
+            int min_pos = 0;
+            while(present== 1){
+                for(int i = 1; i < n; i++){
+                    if(row[i] < row[min_pos] && i!=index){
+                        min_pos = i;
+                    }
+                }
+                present = 0;
+                for(int i = 0; i < n; i++){
+                    if(already_visited[i] == min_pos){
+                        present = 1;
+                        row[min_pos] = DBL_MAX;  //max value for double
+                        min_pos = 0;
+                        break;
+                    }
                 }
             }
-            present = 0;
-            for(int i = 0; i < n; i++){
-                if(already_visited[i] == min_pos){
-                    present = 1;
-                    row[min_pos] = DBL_MAX;  //max value for double
-                    min_pos = 0;
-                    break;
-                }
-            }
+            cost += row[min_pos];
+            already_visited[j] = min_pos;
+            index= min_pos;
         }
-        cost += row[min_pos];
-        already_visited[j] = min_pos;
-        index= min_pos;
+        else{
+            double row[n];
+            memcpy(row, &data->cost_matrix[index*n], n);
+
+
+        }
     }
 
     for(int j = 1; j<n; j++){
