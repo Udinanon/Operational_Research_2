aa97ee4c629307fe7a010a480f733b344cad3575
99lorenzospina <lorenzo.spina@studenti.unipd.it>
2023-03-15 14:59:25 +0100

    added destroy_solution

--- src/tsp_data.h
+++ src/tsp_data.h
@@ -40,4 +40,6 @@ TSP_solution* random_NN(TSP_data* , double ); //NN with randomness
 
 void save_solution(TSP_solution* solution, TSP_data* data, char* problem_filename, char* savename); // can't havce circular dependency with Utility so it's here
 
+void destroy_solution(TSP_solution* ); // deallocate a solution structure
+
 #endif
