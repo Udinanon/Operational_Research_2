88f0e734c1f5475ce786e710fac9c595e9ccfe30
99lorenzospina <lorenzo.spina@studenti.unipd.it>
2023-04-03 19:04:58 +0200

    various things

---
+++ src2/build2/CMakeFiles/tsp.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/chrono.c" "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/build2/CMakeFiles/tsp.dir/chrono.c.o"
+  "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/io_utils.c" "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/build2/CMakeFiles/tsp.dir/io_utils.c.o"
+  "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/main.c" "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/build2/CMakeFiles/tsp.dir/main.c.o"
+  "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/tsp.c" "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/build2/CMakeFiles/tsp.dir/tsp.c.o"
+  "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/tsp_cplex.c" "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/build2/CMakeFiles/tsp.dir/tsp_cplex.c.o"
+  "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/tsp_generative.c" "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/build2/CMakeFiles/tsp.dir/tsp_generative.c.o"
+  "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/tsp_meta.c" "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/build2/CMakeFiles/tsp.dir/tsp_meta.c.o"
+  "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/utils.c" "/home/lorenzo/Op2 with Martino/Operational_Research_2/src2/build2/CMakeFiles/tsp.dir/utils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/opt/ibm/ILOG/CPLEX_Studio2211/cplex/include/ilcplex"
+  "../opt/ibm/ILOG/CPLEX_Studio2211/cplex/include/ilcplex"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src2/build2/CMakeFiles/tsp.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tsp.dir/chrono.c.o"
+  "CMakeFiles/tsp.dir/io_utils.c.o"
+  "CMakeFiles/tsp.dir/main.c.o"
+  "CMakeFiles/tsp.dir/tsp.c.o"
+  "CMakeFiles/tsp.dir/tsp_cplex.c.o"
+  "CMakeFiles/tsp.dir/tsp_generative.c.o"
+  "CMakeFiles/tsp.dir/tsp_meta.c.o"
+  "CMakeFiles/tsp.dir/utils.c.o"
+  "tsp"
+  "tsp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tsp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
